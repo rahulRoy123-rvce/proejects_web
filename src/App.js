c64a56d22ac758c78b3ba07835b13e2d5cedfca3
karthikeya-kotegar <karthikeyakotegar@gmail.com>
2024-04-01 13:38:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Cards from './pages/Projects';
 import Footer from './components/Footer';
+import India from '../src/Assets/india.png';
+import Africa from '../src/Assets/africa.png';
+
+const cardData = [
+  {
+    id: 1,
+    image: India,
+    title: 'India - Odisha & Assam',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+    details: 'Southern Asia\nLasts 100+ years\n€140.00 (VAT excluded)',
+  },
+  {
+    id: 2,
+    image: Africa,
+    title: 'Africa - Ghana',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+    details: 'Africa\nLasts 100+ years\n€140.00 (VAT excluded)',
+  },
+  // Add more card data objects here
+];
+
 
 function App() {
   return (
@@ -15,8 +36,9 @@ function App() {
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
       </p>
-      <h1 className="justify-start ml-60 text-3xl font-bold">Biochar Projects</h1>
-      <Cards/>
+      <h1 className="justify-start text-center text-3xl font-bold my-2">Biochar Projects</h1>
+      <Cards cardData={cardData} />
+
       <Footer/>
      
     </div>
